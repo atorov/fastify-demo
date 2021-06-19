cd01855ed442146cedf0e96fb54204083460f3ca
atorov <veselin.lkp@gmail.com>
2021-06-19 13:50:06 +0300

    added dotenv

--- src/app.js
+++ src/app.js
@@ -1,12 +1,10 @@
 import fastify from 'fastify'
 
-const PORT = 5000 // TODO:
-
 const app = fastify({ logger: true })
 
 async function start() {
     try {
-        await app.listen(PORT)
+        await app.listen(process.env.PORT)
     }
     catch (error) {
         app.log.error(error)
